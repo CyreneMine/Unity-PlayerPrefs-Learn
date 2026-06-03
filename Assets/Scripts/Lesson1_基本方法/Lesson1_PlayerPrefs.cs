58615c28d0923fbaa303636e415de5eeb72ed283
昔涟老公 <a2296720492@gmail.com>
2026-06-03 22:39:09 +0800

    完成 PlayerPrefs 多玩家与排行榜练习

--- Assets/Scripts/Lesson1_基本方法/Lesson1_PlayerPrefs.cs
+++ Assets/Scripts/Lesson1_基本方法/Lesson1_PlayerPrefs.cs
@@ -12,38 +12,37 @@ public class Player
     public int age;
     public int atk;
     public int def;
-    public List<Item> items;
-    
+    public List<Item> items = new List<Item>();
+    private string keyName;
     public void Save()
     {
-        PlayerPrefs.SetString("name", name);
-        PlayerPrefs.SetInt("age", age);
-        PlayerPrefs.SetInt("atk", atk);
-        PlayerPrefs.SetInt("def", def);
-        
-        PlayerPrefs.SetInt("ItemCount", items.Count);
+        PlayerPrefs.SetString(keyName+"_name", name);
+        PlayerPrefs.SetInt(keyName+"_age", age);
+        PlayerPrefs.SetInt(keyName+"_atk", atk);
+        PlayerPrefs.SetInt(keyName+"_def", def);
+        PlayerPrefs.SetInt(keyName+"_ItemCount", items.Count);
         for (int i = 0; i < items.Count; i++)
         {
-            PlayerPrefs.SetInt("itemId"+i, items[i].id);
-            PlayerPrefs.SetInt("itemNum"+i, items[i].num);
+            PlayerPrefs.SetInt(keyName+"_itemId"+i, items[i].id);
+            PlayerPrefs.SetInt(keyName+"_itemNum"+i, items[i].num);
         }
         PlayerPrefs.Save();
     }
 
-    public void Load()
+    public void Load(string keyName)
     {
-        name = PlayerPrefs.GetString("name","Cyrene");
-        age = PlayerPrefs.GetInt("age",20);
-        atk = PlayerPrefs.GetInt("atk",99999);
-        def = PlayerPrefs.GetInt("def",99999);
-
-        items = new List<Item>();
+        items.Clear();
+        this.keyName = keyName;
+        name = PlayerPrefs.GetString(keyName+"_name","Cyrene");
+        age = PlayerPrefs.GetInt(keyName+"_age",20);
+        atk = PlayerPrefs.GetInt(keyName+"_atk",99999);
+        def = PlayerPrefs.GetInt(keyName+"_def",99999);
         
-        for (int i = 0; i < PlayerPrefs.GetInt("ItemCount"); i++)
+        for (int i = 0; i < PlayerPrefs.GetInt(keyName+"_ItemCount"); i++)
         {
             Item item = new Item();
-            item.id = PlayerPrefs.GetInt("itemId"+i);
-            item.num = PlayerPrefs.GetInt("itemNum"+i);
+            item.id = PlayerPrefs.GetInt(keyName+"_itemId"+i);
+            item.num = PlayerPrefs.GetInt(keyName+"_itemNum"+i);
             items.Add(item);
         }
         
@@ -69,24 +68,32 @@ public class Lesson1_PlayerPrefs : MonoBehaviour
     void Start()
     {
         Player p = new Player();
-        p.Load();
-        p.Show();
-        p.name = "CyreneMine";
-        p.age = 22;
+        p.Load("Player1");
         p.Save();
-        
-        
-        Item item = new Item();
-        item.id = 1;
-        item.num = 1;
-        p.items.Add(item);
-        item = new Item();
-        item.id = 2;
-        item.num = 2;
-        p.items.Add(item);
-        
         p.Show();
-        p.Save();
+        
+        Player p2 = new Player();
+        p2.Load("Player2");
+        // p2.name = "CyreneMine";
+        p2.Save();
+        p2.Show();
+        
+        // p.name = "CyreneMine";
+        // p.age = 22;
+        // p.Save();
+        //
+        //
+        // Item item = new Item();
+        // item.id = 1;
+        // item.num = 1;
+        // p.items.Add(item);
+        // item = new Item();
+        // item.id = 2;
+        // item.num = 2;
+        // p.items.Add(item);
+        //
+        // p.Show();
+        
         // PlayerPrefs.DeleteAll();
     }
     
---
+++ Assets/Scripts/Lesson2/Lesson2.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RankListInfo
+{
+    public List<RankInfo> ranks;
+
+    public void updateRank(RankInfo newRank)
+    {
+        bool flag = true;
+        for (int i = 0; i < ranks.Count; i++)
+        {
+            if (ranks[i].playerName == newRank.playerName)
+            {
+                flag = false;
+                if (ranks[i].playerScore < newRank.playerScore)
+                {
+                    ranks[i].playerScore = newRank.playerScore;
+                    if (ranks[i].playerTime > newRank.playerTime)
+                    {
+                        ranks[i].playerTime = newRank.playerTime;
+                    }
+                }
+                break;
+            }
+        }
+
+        if (flag)
+        {
+            ranks.Add(newRank);
+        }
+    }
+    public void Save()
+    {
+        PlayerPrefs.SetInt("rankListNum", ranks.Count);
+        for (int i = 0; i < ranks.Count; i++)
+        {
+            PlayerPrefs.SetString("rankInfo"+i, ranks[i].playerName);
+            PlayerPrefs.SetInt("rankScore"+i,ranks[i].playerScore);
+            PlayerPrefs.SetInt("rankTime"+i,ranks[i].playerTime);
+        }
+        PlayerPrefs.Save();
+    }
+
+    public void Load()
+    {
+        ranks = new List<RankInfo>();
+        int rankCount = PlayerPrefs.GetInt("rankListNum");
+        for (int i = 0; i < rankCount; i++)
+        {
+            string str = PlayerPrefs.GetString("rankInfo"+i, "");
+            int score = PlayerPrefs.GetInt("rankScore"+i, 0);
+            int time = PlayerPrefs.GetInt("rankTime"+i, 0);
+            RankInfo temp = new RankInfo(str,score,time);
+            ranks.Add(temp);
+        }
+    }
+    public  void Show()
+    {
+        ranks.Sort((a, b) =>
+        {
+            return b.playerScore.CompareTo(a.playerScore);
+        });
+        for (int i = 0; i < ranks.Count; i++)
+        {
+            Debug.Log($"第{i+1}名:{ranks[i].playerName}\t分数:{ranks[i].playerScore}\t通关时间:{ranks[i].playerTime}");
+        }
+    }
+}
+
+public class RankInfo
+{
+    public string playerName;
+    public int playerScore;
+    public int playerTime;
+   public RankInfo(string playerName, int playerScore, int playerTime)
+    {
+        this.playerName = playerName;
+        this.playerScore = playerScore;
+        this.playerTime = playerTime;
+    }
+}
+public class Lesson2 : MonoBehaviour
+{
+    RankListInfo rankListInfo = new RankListInfo();
+    private void Awake()
+    {
+        rankListInfo.Load();
+        RankInfo r1 = new RankInfo("TestDate", 10, 50);
+        RankInfo r2 = new RankInfo("TestDate2", 2, 52);
+        rankListInfo.updateRank(r1);
+        rankListInfo.updateRank(r2);
+        rankListInfo.Save();
+        
+        rankListInfo.Show();
+    }
+
+    
+}
